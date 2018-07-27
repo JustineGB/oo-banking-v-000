52fc7469c3dbcd1640743a30316023bc02dfc2bc
Justine Bricknell <justinegayton@gmail.com>
2018-07-27 18:33:47 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -23,7 +23,7 @@ class Transfer #money transfers btw two bank accounts
     #how to call on the bankaccount class without passing in an argument here?!?!? maybe I can make sender and reciever attr_reader's in bank account and then I can use that method???
 
   def execute_transaction
-    if self.valid? == false && @sender.balance < @amount
+    if self.valid? == false #&& @sender.balance < @amount
       self.status = "rejected"
       return "Transaction rejected. Please check your account balance."
     elsif self.valid? == true && self.status != "complete"
